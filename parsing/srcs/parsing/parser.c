fb321314aa93949955b243ab82a43b375ee2b084
yaasinslami <yassinx4002@gmail.com>
2025-02-28 13:48:05 +0000

    make parsing directory - to be merged

--- parsing/srcs/parsing/parser.c
+++ parsing/srcs/parsing/parser.c
@@ -6,6 +6,101 @@ static int	end_with_operator(char *line);
 static void	give_type(t_token **token);
 static int	handle_end_of_line(char **line, t_token **token);
 
+void	process_input(char *line, t_token **token)
+{
+	t_tree	*tree;
+	t_vars	*vars;
+
+	init_vars(&vars, line);
+	init_token(token, 0);
+	if (tokenizer(token, vars) == 0)
+	{
+		give_type(token);
+		if (!check_syntax(*token, 0))
+			return (add_history(line), (void)0);
+		if (handle_end_of_line(&line, token))
+			return ;
+		if (line)
+			add_history(line);
+		tree = build_ast(*token);
+		// printf("\n===== AST Structure =====\n");
+		// print_ast(tree, 0, "Root");
+		// printf("=========================\n\n");
+	}
+}
+
+static int	tokenizer(t_token **token, t_vars *vars)
+{
+	int	ret;
+
+	ret = 0;
+	while (vars->cmd[vars->i] && !ret)
+	{
+		if (is_space(vars->cmd[vars->i]))
+			ft_space(&vars, &ret);
+		else if (isquote(vars->cmd[vars->i]))
+			parse_quote(token, &vars, &ret);
+		else if (vars->cmd[vars->i] == '$')
+			parse_dollar(token, &vars, &ret);
+		else if (special_d(vars->cmd[vars->i]))
+			parse_separator(token, &vars, &ret);
+		else if (isparenth(vars->cmd[vars->i]))
+			parse_parenthesis(token, &vars, &ret);
+		else
+			parse_char(token, &vars, &ret);
+	}
+	return (ret);
+}
+
+static void	give_type(t_token **token)
+{
+	t_token	*curr;
+
+	curr = *token;
+	while (curr)
+	{
+		if (curr->value && *curr->value)
+			curr->type = get_type(curr->value);
+		curr = curr->next;
+	}
+}
+
+static int end_with_operator(char *line)
+{
+	size_t	len;
+
+	if (!line || !*line)
+		return (0);
+	len = ft_strlen_0(line);
+	while (len > 0 && (line[len -1] == ' ' || line[len - 1] == '\t'))
+		len--;
+	if (len >= 1 && line[len - 1] == '|')
+		return (1);
+	if (len >= 2 && ft_strncmp(&line[len - 2], "&&", 2) == 0)
+		return (1);
+	if (len >= 2 && ft_strncmp(&line[len - 2], "||", 2) == 0)
+		return (1);
+	return (0);
+}
+
+static int	handle_end_of_line(char **line, t_token **token)
+{
+	char	*new_line;
+
+	while (end_with_operator(*line))
+	{
+		new_line = readline("> ");
+		if (!new_line)
+			return (ft_printf("Error\nReadline Error!\n"), 1);
+		*line = ft_strjoin(*line, " ");
+		*line = ft_strjoin(*line, new_line);
+		free(new_line);
+		process_input(*line, token);
+		return (1);
+	}
+	return (0);
+}
+
 /*
 const char *type_names[] = {
    "T_AND",
@@ -116,98 +211,3 @@ static void print_ast(t_tree *node, int depth, const char *relation)
         print_ast(node->right, depth + 1, "Right");
 }
 */
-
-void	process_input(char *line, t_token **token)
-{
-	t_tree	*tree;
-	t_vars	*vars;
-
-	init_vars(&vars, line);
-	init_token(token, 0);
-	if (tokenizer(token, vars) == 0)
-	{
-		give_type(token);
-		if (!check_syntax(*token, 0))
-			return (add_history(line), (void)0);
-		if (handle_end_of_line(&line, token))
-			return ;
-		if (line)
-			add_history(line);
-		tree = build_ast(*token);
-		// printf("\n===== AST Structure =====\n");
-		// print_ast(tree, 0, "Root");
-		// printf("=========================\n\n");
-	}
-}
-
-static int	tokenizer(t_token **token, t_vars *vars)
-{
-	int	ret;
-
-	ret = 0;
-	while (vars->cmd[vars->i] && !ret)
-	{
-		if (is_space(vars->cmd[vars->i]))
-			ft_space(&vars, &ret);
-		else if (isquote(vars->cmd[vars->i]))
-			parse_quote(token, &vars, &ret);
-		else if (vars->cmd[vars->i] == '$')
-			parse_dollar(token, &vars, &ret);
-		else if (special_d(vars->cmd[vars->i]))
-			parse_separator(token, &vars, &ret);
-		else if (isparenth(vars->cmd[vars->i]))
-			parse_parenthesis(token, &vars, &ret);
-		else
-			parse_char(token, &vars, &ret);
-	}
-	return (ret);
-}
-
-static void	give_type(t_token **token)
-{
-	t_token	*curr;
-
-	curr = *token;
-	while (curr)
-	{
-		if (curr->value && *curr->value)
-			curr->type = get_type(curr->value);
-		curr = curr->next;
-	}
-}
-
-static int end_with_operator(char *line)
-{
-	size_t	len;
-
-	if (!line || !*line)
-		return (0);
-	len = ft_strlen_0(line);
-	while (len > 0 && (line[len -1] == ' ' || line[len - 1] == '\t'))
-		len--;
-	if (len >= 1 && line[len - 1] == '|')
-		return (1);
-	if (len >= 2 && ft_strncmp(&line[len - 2], "&&", 2) == 0)
-		return (1);
-	if (len >= 2 && ft_strncmp(&line[len - 2], "||", 2) == 0)
-		return (1);
-	return (0);
-}
-
-static int	handle_end_of_line(char **line, t_token **token)
-{
-	char	*new_line;
-
-	while (end_with_operator(*line))
-	{
-		new_line = readline("> ");
-		if (!new_line)
-			return (ft_printf("Error\nReadline Error!\n"), 1);
-		*line = ft_strjoin(*line, " ");
-		*line = ft_strjoin(*line, new_line);
-		free(new_line);
-		process_input(*line, token);
-		return (1);
-	}
-	return (0);
-}
