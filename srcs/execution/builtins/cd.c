b56188b150e89eb580108f4d660fd92f31c74af3
oussama-el1 <elhadraoui284@gmail.com>
2025-03-07 18:19:45 +0000

    solve chdir error in cd

--- srcs/execution/builtins/cd.c
+++ srcs/execution/builtins/cd.c
@@ -16,7 +16,7 @@ static int	change_home_dir(char **argv, t_env *env, int exit_status)
 {
 	char	*home;
 
-	if (argv[1] == NULL)
+	if (argv[1] == NULL || (!ft_strcmp(argv[1], "~") && !argv[2]))
 	{
 		home = get_env_var(env, "HOME", exit_status);
 		if (!home)
@@ -36,17 +36,23 @@ static int	change_home_dir(char **argv, t_env *env, int exit_status)
 	return (-1);
 }
 
-static void	update_env(char *old_pwd, t_env *env)
+static int	update_env(char **argv, char *old_pwd, t_env *env)
 {
 	char	*new_pwd;
 
 	new_pwd = getcwd(NULL, 0);
-	if (new_pwd)
+	if (!new_pwd)
 	{
+		ft_putstr_fd("chdir: error retrieving current directory: getcwd: cannot access parent directories: No such file or directory\n", 2);
 		set_env_var(&env, ft_strdup("OLDPWD", ENV), ft_strdup(old_pwd, ENV), 1);
-		set_env_var(&env, ft_strdup("PWD", ENV), ft_strdup(new_pwd, ENV), 1);
+		change_home_dir(argv, env, 0);
+		set_env_var(&env, ft_strdup("PWD", ENV), ft_strdup(get_env_var(env, "HOME", 0), ENV), 1);
+		return (1);
 	}
+	set_env_var(&env, ft_strdup("OLDPWD", ENV), ft_strdup(old_pwd, ENV), 1);
+	set_env_var(&env, ft_strdup("PWD", ENV), ft_strdup(new_pwd, ENV), 1);
 	free(new_pwd);
+	return (0);
 }
 
 int	handle_oldpwd(char *oldpwd)
@@ -77,7 +83,12 @@ int	ft_cd(char **argv, t_env *env, int exit_status)
 	if (home_res == 1)
 		return (1);
 	if (home_res == 0)
-		return (update_env(old_pwd, env), 0);
+		return (update_env(argv, old_pwd, env), 0);
+	if (argv[1] && argv[2])
+	{
+		ft_putstr_fd("minishell: cd: too many arguments\n", 2);
+		return (1);
+	}
 	if (argv[1] && !ft_strcmp(argv[1], "-"))
 	{
 		oldpwd = get_env_var(env, "OLDPWD", exit_status);
@@ -91,6 +102,6 @@ int	ft_cd(char **argv, t_env *env, int exit_status)
 		ft_putstr_fd(argv[1], 2);
 		return (ft_putstr_fd(": No such file or directory\n", 2), 1);
 	}
-	update_env(old_pwd, env);
-	return (0);
+
+	return (update_env(argv, old_pwd, env));
 }
