5cd073a79e5f8bb70e5ec801a92c2887afbbe9ea
yaasinslami <yassinx4002@gmail.com>
2025-03-08 20:15:42 +0000

    fix wildcard checking

--- srcs/utils/list_utils2.c
+++ srcs/utils/list_utils2.c
@@ -6,7 +6,7 @@
 /*   By: yslami <yslami@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 13:58:51 by yslami            #+#    #+#             */
-/*   Updated: 2025/03/08 17:28:48 by yslami           ###   ########.fr       */
+/*   Updated: 2025/03/08 20:04:58 by yslami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,17 +45,15 @@ char	*quoted_process(t_token **curr, t_expand **expansion_list, bool *wildcard)
 	char		*tmp;
 	char		*arg;
 	t_expand	*head;
-	t_token		*save;
 	size_t		start;
-	int			level;
 
 	head = NULL;
 	arg = NULL;
 	start = 0;
-	level = 0;
+	*wildcard = false;
 	while (*curr && (*curr)->visited != 1 && non_control((*curr)->type))
 	{
-		save = *curr;
+		is_wildcard(*curr, wildcard);
 		if (*curr)
 			tmp = (*curr)->value;
 		expansion_func(&head, *curr, &tmp, &start);
@@ -67,14 +65,6 @@ char	*quoted_process(t_token **curr, t_expand **expansion_list, bool *wildcard)
 		*curr = (*curr)->next;
 		if (!*curr || (*curr)->visited == 1 || (*curr)->bef_space == 1)
 			break ;
-		level = 1;
-	}
-	if (!level && is_wildcard(save) && wildcard)
-		*wildcard = true;
-	else
-	{
-		if (wildcard)
-			*wildcard = false;
 	}
 	*expansion_list = head;
 	return (arg);
