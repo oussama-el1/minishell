36c70fd4ec1cb438d339f2e9eed9f2be5e491960
yaasinslami <yassinx4002@gmail.com>
2025-03-17 00:25:26 +0000

    handle non tty

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: yslami <yslami@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 11:08:50 by oel-hadr          #+#    #+#             */
-/*   Updated: 2025/03/16 08:52:08 by yslami           ###   ########.fr       */
+/*   Updated: 2025/03/17 00:22:22 by yslami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(int ac, char **av, char **envp)
 	((void)ac, (void)av);
 	env = init_env(envp);
 	bash_loop(&env);
-	return (0);
+	return (g_signals.exit_status);
 }
 
 static void	bash_loop(t_env **env)
@@ -30,9 +30,14 @@ static void	bash_loop(t_env **env)
 	t_token		*token;
 	t_helper	hp;
 
+	if (!isatty(STDIN_FILENO))
+    {
+        ft_putendl_fd("Error: minishell must be run in an interactive terminal.", 2);
+		g_signals.exit_status = 1;
+		return ;
+    }
 	init_setup(&hp, env);
 	setup_signals(&hp);
-	// rl_catch_signals = 0;
 	while (1)
 	{
 		init_token(&token, 1);
