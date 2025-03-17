a8e2d9e036624e15bd7e317b23c2f3d5e27d0b92
yaasinslami <yassinx4002@gmail.com>
2025-03-17 10:27:33 +0000

    clean things

--- srcs/helper/ft_strtrim.c
+++ srcs/helper/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: yslami <yslami@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 12:14:02 by yslami            #+#    #+#             */
-/*   Updated: 2025/03/17 06:31:10 by yslami           ###   ########.fr       */
+/*   Updated: 2025/03/17 10:26:58 by yslami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ char	*ft_strtrim(char const *s1, char const *set, int type)
 		++start;
 	if (start > 1)
 		return (ft_strdup("", CMD));
-	while (end >= 0 && s1[end] && ft_strrchr(set, s1[end]))
+	while (s1[end] && ft_strrchr(set, s1[end]))
 		--end;
 	trimmed = ft_substr(s1, start, end - start + 1, type);
 	return (trimmed);
